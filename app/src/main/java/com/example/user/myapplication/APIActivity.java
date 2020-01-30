79b322ae20fcccaff284c6a88d1716096501b21a
Maor Yatskan <mymaor89@gmail.com>
2020-01-30 15:29:49 +0200

    Basic Volley request to google (added Volley dependency in build.gradle)

--- app/src/main/java/com/example/user/myapplication/APIActivity.java
+++ app/src/main/java/com/example/user/myapplication/APIActivity.java
@@ -2,12 +2,52 @@ package com.example.user.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class APIActivity extends AppCompatActivity {
+    TextView textView;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_api);
+        textView = (TextView) findViewById(R.id.tv_result);
+        btn = (Button) findViewById(R.id.btn_request);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendRequest();
+            }
+        });
+    }
+    private void sendRequest(){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
