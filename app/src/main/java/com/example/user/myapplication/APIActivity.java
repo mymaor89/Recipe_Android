9b6c518d3dd828e0e92bda8485e68bbf40aa0f50
Maor Yatskan <mymaor89@gmail.com>
2020-01-30 21:07:00 +0200

    Showing image from url using Picasso

--- app/src/main/java/com/example/user/myapplication/APIActivity.java
+++ app/src/main/java/com/example/user/myapplication/APIActivity.java
@@ -1,10 +1,12 @@
 package com.example.user.myapplication;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -13,6 +15,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,6 +29,7 @@ public class APIActivity extends AppCompatActivity {
     EditText edt;
     String query;
     String url;
+    ImageView imageView;
     //DOCS : https://developer.edamam.com/edamam-docs-recipe-api
 
     @Override
@@ -35,6 +39,7 @@ public class APIActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.tv_result);
         btn = (Button) findViewById(R.id.btn_request);
         edt = (EditText) findViewById(R.id.et_params);
+        imageView = (ImageView) findViewById(R.id.imageView);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,8 +55,7 @@ public class APIActivity extends AppCompatActivity {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                      //  textView.setText("Response is: "+ response.substring(0,500));
+                        
                         try {
                             JSONObject jObject = new JSONObject(response);
                             JSONArray  jsonArray  = jObject.getJSONArray("hits");
@@ -59,6 +63,8 @@ public class APIActivity extends AppCompatActivity {
                             jObject = jObject.getJSONObject("recipe");
                             String title = jObject.getString("label");
                             textView.setText(title);
+                            String image_URL = jObject.getString("image");
+                            Picasso.with(getApplicationContext()).load(image_URL).into(imageView);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
