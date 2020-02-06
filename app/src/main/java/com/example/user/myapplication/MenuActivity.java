e45e0bcdbed04618d84b963e47be5b2c394c23c2
Maor Yatskan <mymaor89@gmail.com>
2020-02-06 20:45:58 +0200

    Search button navigate to APIActivity

--- app/src/main/java/com/example/user/myapplication/MenuActivity.java
+++ app/src/main/java/com/example/user/myapplication/MenuActivity.java
@@ -9,10 +9,19 @@ import android.widget.ImageButton;
 
 public class MenuActivity extends AppCompatActivity {
     ImageButton btn_creme,btn_cookie;
+    Button btn_search;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        btn_search = (Button) findViewById(R.id.btn_search);
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, APIActivity.class);
+                startActivity(intent);
+            }
+        });
         btn_creme = (ImageButton) findViewById(R.id.creme);
         btn_cookie = (ImageButton) findViewById(R.id.cookie);
         btn_creme.setOnClickListener(new View.OnClickListener() {
